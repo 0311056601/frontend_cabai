a6d0da0e45e290f04b6972a6b124fd6ec9a574c5
Shulhan Akmal <shulhanakmal@gmail.com>
2022-11-20 09:31:38 +0700

    update tampilan login

--- src/components/login.js
+++ src/components/login.js
@@ -88,137 +88,137 @@ class Login extends Component {
     }
   }
 
-  componentDidMount() {
+  // componentDidMount() {
 
-    this.setState({
-      loading: true,
-    });
+    // this.setState({
+    //   loading: true,
+    // });
 
-    UserService.getDashboardLogin().then((response) => { 
-      if(response.data) {
+    // UserService.getDashboardLogin().then((response) => { 
+    //   if(response.data) {
 
-        /* Chart code */
-        // Themes begin
-        am4core.useTheme(am4themes_animated);
-        // Themes end
+    //     /* Chart code */
+    //     // Themes begin
+    //     am4core.useTheme(am4themes_animated);
+    //     // Themes end
 
-        // Create chart instance
-        let chart = am4core.create("chartdivExternal", am4charts.XYChart);
+    //     // Create chart instance
+    //     let chart = am4core.create("chartdivExternal", am4charts.XYChart);
 
-        // Export
-        chart.exporting.menu = new am4core.ExportMenu();
+    //     // Export
+    //     chart.exporting.menu = new am4core.ExportMenu();
 
-        /* Create axes */
-        let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.dataFields.category = "tanggal";
-        categoryAxis.renderer.minGridDistance = 30;
+    //     /* Create axes */
+    //     let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    //     categoryAxis.dataFields.category = "tanggal";
+    //     categoryAxis.renderer.minGridDistance = 30;
 
-        /* Create value axis */
-        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    //     /* Create value axis */
+    //     let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-        /* Create series */
-        let columnSeries = chart.series.push(new am4charts.ColumnSeries());
-        columnSeries.name = "produk";
-        columnSeries.dataFields.valueY = "produk";
-        columnSeries.dataFields.categoryX = "tanggal";
+    //     /* Create series */
+    //     let columnSeries = chart.series.push(new am4charts.ColumnSeries());
+    //     columnSeries.name = "produk";
+    //     columnSeries.dataFields.valueY = "produk";
+    //     columnSeries.dataFields.categoryX = "tanggal";
 
-        columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
-        columnSeries.columns.template.propertyFields.fillOpacity = "fillOpacity";
-        columnSeries.columns.template.propertyFields.stroke = "stroke";
-        columnSeries.columns.template.propertyFields.strokeWidth = "strokeWidth";
-        columnSeries.columns.template.propertyFields.strokeDasharray = "columnDash";
-        columnSeries.tooltip.label.textAlign = "middle";
+    //     columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
+    //     columnSeries.columns.template.propertyFields.fillOpacity = "fillOpacity";
+    //     columnSeries.columns.template.propertyFields.stroke = "stroke";
+    //     columnSeries.columns.template.propertyFields.strokeWidth = "strokeWidth";
+    //     columnSeries.columns.template.propertyFields.strokeDasharray = "columnDash";
+    //     columnSeries.tooltip.label.textAlign = "middle";
 
-        let lineSeries = chart.series.push(new am4charts.LineSeries());
-        lineSeries.name = "kebutuhan";
-        lineSeries.dataFields.valueY = "kebutuhan";
-        lineSeries.dataFields.categoryX = "tanggal";
+    //     let lineSeries = chart.series.push(new am4charts.LineSeries());
+    //     lineSeries.name = "kebutuhan";
+    //     lineSeries.dataFields.valueY = "kebutuhan";
+    //     lineSeries.dataFields.categoryX = "tanggal";
 
-        lineSeries.stroke = am4core.color("#fdd400");
-        lineSeries.strokeWidth = 3;
-        lineSeries.propertyFields.strokeDasharray = "lineDash";
-        lineSeries.tooltip.label.textAlign = "middle";
+    //     lineSeries.stroke = am4core.color("#fdd400");
+    //     lineSeries.strokeWidth = 3;
+    //     lineSeries.propertyFields.strokeDasharray = "lineDash";
+    //     lineSeries.tooltip.label.textAlign = "middle";
 
-        let bullet = lineSeries.bullets.push(new am4charts.Bullet());
-        bullet.fill = am4core.color("#fdd400"); // tooltips grab fill from parent by default
-        bullet.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
-        let circle = bullet.createChild(am4core.Circle);
-        circle.radius = 4;
-        circle.fill = am4core.color("#fff");
-        circle.strokeWidth = 3;
+    //     let bullet = lineSeries.bullets.push(new am4charts.Bullet());
+    //     bullet.fill = am4core.color("#fdd400"); // tooltips grab fill from parent by default
+    //     bullet.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
+    //     let circle = bullet.createChild(am4core.Circle);
+    //     circle.radius = 4;
+    //     circle.fill = am4core.color("#fff");
+    //     circle.strokeWidth = 3;
 
-        chart.data = response.data.data.sort((a, b) => (b.no > a.no) ? 1 : -1);
+    //     chart.data = response.data.data.sort((a, b) => (b.no > a.no) ? 1 : -1);
 
-        this.setState({
-          data: response.data.data,
-        });
+    //     this.setState({
+    //       data: response.data.data,
+    //     });
 
-      } else {
+    //   } else {
 
-        /* Chart code */
-        // Themes begin
-        am4core.useTheme(am4themes_animated);
-        // Themes end
+    //     /* Chart code */
+    //     // Themes begin
+    //     am4core.useTheme(am4themes_animated);
+    //     // Themes end
 
-        // Create chart instance
-        let chart = am4core.create("chartdivExternal", am4charts.XYChart);
+    //     // Create chart instance
+    //     let chart = am4core.create("chartdivExternal", am4charts.XYChart);
 
-        // Export
-        chart.exporting.menu = new am4core.ExportMenu();
+    //     // Export
+    //     chart.exporting.menu = new am4core.ExportMenu();
 
-        /* Create axes */
-        let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.dataFields.category = "tanggal";
-        categoryAxis.renderer.minGridDistance = 30;
+    //     /* Create axes */
+    //     let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    //     categoryAxis.dataFields.category = "tanggal";
+    //     categoryAxis.renderer.minGridDistance = 30;
 
-        /* Create value axis */
-        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    //     /* Create value axis */
+    //     let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-        /* Create series */
-        let columnSeries = chart.series.push(new am4charts.ColumnSeries());
-        columnSeries.name = "produk";
-        columnSeries.dataFields.valueY = "produk";
-        columnSeries.dataFields.categoryX = "tanggal";
+    //     /* Create series */
+    //     let columnSeries = chart.series.push(new am4charts.ColumnSeries());
+    //     columnSeries.name = "produk";
+    //     columnSeries.dataFields.valueY = "produk";
+    //     columnSeries.dataFields.categoryX = "tanggal";
 
-        columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
-        columnSeries.columns.template.propertyFields.fillOpacity = "fillOpacity";
-        columnSeries.columns.template.propertyFields.stroke = "stroke";
-        columnSeries.columns.template.propertyFields.strokeWidth = "strokeWidth";
-        columnSeries.columns.template.propertyFields.strokeDasharray = "columnDash";
-        columnSeries.tooltip.label.textAlign = "middle";
+    //     columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
+    //     columnSeries.columns.template.propertyFields.fillOpacity = "fillOpacity";
+    //     columnSeries.columns.template.propertyFields.stroke = "stroke";
+    //     columnSeries.columns.template.propertyFields.strokeWidth = "strokeWidth";
+    //     columnSeries.columns.template.propertyFields.strokeDasharray = "columnDash";
+    //     columnSeries.tooltip.label.textAlign = "middle";
 
-        let lineSeries = chart.series.push(new am4charts.LineSeries());
-        lineSeries.name = "kebutuhan";
-        lineSeries.dataFields.valueY = "kebutuhan";
-        lineSeries.dataFields.categoryX = "tanggal";
+    //     let lineSeries = chart.series.push(new am4charts.LineSeries());
+    //     lineSeries.name = "kebutuhan";
+    //     lineSeries.dataFields.valueY = "kebutuhan";
+    //     lineSeries.dataFields.categoryX = "tanggal";
 
-        lineSeries.stroke = am4core.color("#fdd400");
-        lineSeries.strokeWidth = 3;
-        lineSeries.propertyFields.strokeDasharray = "lineDash";
-        lineSeries.tooltip.label.textAlign = "middle";
+    //     lineSeries.stroke = am4core.color("#fdd400");
+    //     lineSeries.strokeWidth = 3;
+    //     lineSeries.propertyFields.strokeDasharray = "lineDash";
+    //     lineSeries.tooltip.label.textAlign = "middle";
 
-        let bullet = lineSeries.bullets.push(new am4charts.Bullet());
-        bullet.fill = am4core.color("#fdd400"); // tooltips grab fill from parent by default
-        bullet.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
-        let circle = bullet.createChild(am4core.Circle);
-        circle.radius = 4;
-        circle.fill = am4core.color("#fff");
-        circle.strokeWidth = 3;
+    //     let bullet = lineSeries.bullets.push(new am4charts.Bullet());
+    //     bullet.fill = am4core.color("#fdd400"); // tooltips grab fill from parent by default
+    //     bullet.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
+    //     let circle = bullet.createChild(am4core.Circle);
+    //     circle.radius = 4;
+    //     circle.fill = am4core.color("#fff");
+    //     circle.strokeWidth = 3;
 
-        chart.data = [];
+    //     chart.data = [];
 
-        this.setState({
-          data: []
-        });
+    //     this.setState({
+    //       data: []
+    //     });
 
-      }
+    //   }
 
-      this.setState({
-        loading: false,
-      });
-    })
+    //   this.setState({
+    //     loading: false,
+    //   });
+    // })
 
-  }
+  // }
 
   render() {
     const { isLoggedIn, message } = this.props;
@@ -232,18 +232,8 @@ class Login extends Component {
         <CContainer>
 
           <CRow className="justify-content-center">
-            <CCol md="12">
 
-              <br></br>
-              <br></br>
-
-              <CCardGroup>
-
-                <CCard>
-                  <div id="chartdivExternal"></div>
-                </CCard>
-
-                <CCard className="p-4 col-md-4">
+          <CCard className="p-6 col-md-6">
                   <CCardBody>
                     <h1>Masuk</h1>
                     <p className="text-muted">masuk dengan akunmu, jika belum memiliki akun, silahkan daftar terlebih dahulu</p>
@@ -318,9 +308,22 @@ class Login extends Component {
                   </CCardBody>
                 </CCard>
 
-              </CCardGroup>
+            {/* <CCol md="12"> */}
 
-            </CCol>
+              {/* <br></br>
+              <br></br> */}
+
+              {/* <CCardGroup>
+
+                <CCard>
+                  <div id="chartdivExternal"></div>
+                </CCard>
+
+                
+
+              </CCardGroup> */}
+
+            {/* </CCol> */}
           </CRow>
         </CContainer>
       </div>
